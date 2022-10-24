9ff6eb6edb543a6e72436e029e8780b2d8dbf16b
Pulavarthy Karthik <kpulavarthy0910@gmail.com>
2022-10-24 21:02:08 +0530

    changed synapse name

--- modules/Synapse/main.tf
+++ modules/Synapse/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "example" {
-  name                     = "demostorageaccdepadlssynapse"
+  name                     = "demostradlssynapse"
   resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = "Standard"
